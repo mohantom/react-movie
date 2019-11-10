928538250682fc81b9aba82e0168c11f8bf3302a
Wei Tang <wtang6@gmail.com>
2019-11-10 09:41:40 -0500

    add router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React, { useEffect } from "react";
+import React, {  Suspense } from "react";
+import { Route, Switch, Redirect } from 'react-router-dom';
 import "bulma/css/bulma.css";
 import "./App.css";
 import MovieHome from './views/MovieHome'
 
 function App() {
+  const routes = (
+    <Switch>
+      <Route path="/home" render={props => <MovieHome {...props} />} />
+      <Route path="/" exact component={MovieHome} />
+      <Redirect to="/" />
+    </Switch>
+  );
+
   return (
     <div className="App">
-      <MovieHome />
+      <div>
+        <Suspense fallback={<p>Loading...</p>}>{routes}</Suspense>
+      </div>
     </div>
   );
 }
