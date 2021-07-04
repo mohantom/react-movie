0ff8091915bdd0073a8b14efc7e58c912a80e2bf
mohantom <mohantom@users.noreply.github.com>
2021-07-04 02:51:00 +0000

    deploy: d99b87945dc2f6c3aa3d1ffad6a98d7334a8b9f4

---
+++ precache-manifest.2d207837ec4bf26af46faa43293c8d2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b67a6da4b1eb203c9c43a20a2c385f0a",
+    "url": "/react-movie/index.html"
+  },
+  {
+    "revision": "6c26d5b13025cab0da63",
+    "url": "/react-movie/static/css/2.6c86b6e9.chunk.css"
+  },
+  {
+    "revision": "557e68d11880a8df9e10",
+    "url": "/react-movie/static/css/main.8c169cd0.chunk.css"
+  },
+  {
+    "revision": "6c26d5b13025cab0da63",
+    "url": "/react-movie/static/js/2.ce5f444d.chunk.js"
+  },
+  {
+    "revision": "557e68d11880a8df9e10",
+    "url": "/react-movie/static/js/main.0cc15bf7.chunk.js"
+  },
+  {
+    "revision": "12e4347db285fbd32804",
+    "url": "/react-movie/static/js/runtime-main.0e207807.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.734172b3f0092499a621a746c58e44e5.js"
+  "/react-movie/precache-manifest.2d207837ec4bf26af46faa43293c8d2a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-movie/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
